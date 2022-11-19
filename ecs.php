beef5608bcb5624ac1da3b0fb899e5431d85b976
Tom Wright <tom@inflatablecookie.com>
2022-11-19 14:02:00 +0000

    Updated CI workflow
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- ecs.php
+++ ecs.php
@@ -8,6 +8,6 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([__DIR__ . '/src']);
+    $ecsConfig->paths([__DIR__.'/src', __DIR__.'/stubs']);
     $ecsConfig->sets([SetList::CLEAN_CODE, SetList::PSR_12]);
 };
--- stubs/DecodeLabs/Archetype.php
+++ stubs/DecodeLabs/Archetype.php
@@ -5,30 +5,37 @@
  */
 namespace DecodeLabs;
 
-use DecodeLabs\Veneer\Proxy;
-use DecodeLabs\Veneer\ProxyTrait;
 use DecodeLabs\Archetype\Handler as Inst;
 use DecodeLabs\Archetype\Resolver as Ref0;
+use DecodeLabs\Veneer\Proxy;
+use DecodeLabs\Veneer\ProxyTrait;
 use Generator as Ref1;
 
 class Archetype implements Proxy
 {
     use ProxyTrait;
 
-    const VENEER = 'DecodeLabs\Archetype';
-    const VENEER_TARGET = Inst::class;
+    public const VENEER = 'DecodeLabs\Archetype';
+    public const VENEER_TARGET = Inst::class;
 
     public static Inst $instance;
 
-    public static function register(Ref0 $resolver): void {}
-    public static function unregister(Ref0 $resolver): void {}
-    public static function resolve(string $interface, string $name): string {
+    public static function register(Ref0 $resolver): void
+    {
+    }
+    public static function unregister(Ref0 $resolver): void
+    {
+    }
+    public static function resolve(string $interface, string $name): string
+    {
         return static::$instance->resolve(...func_get_args());
     }
-    public static function findFile(string $interface, string $name): string {
+    public static function findFile(string $interface, string $name): string
+    {
         return static::$instance->findFile(...func_get_args());
     }
-    public static function scanClasses(string $interface): Ref1 {
+    public static function scanClasses(string $interface): Ref1
+    {
         return static::$instance->scanClasses(...func_get_args());
     }
-};
+}
